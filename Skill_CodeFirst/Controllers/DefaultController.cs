436b0a650da48d2e78db92e453f10dacf195bf81
Hatice Albayrak <haticekbr17@gmail.com>
2022-12-28 16:53:01 +0300

    Add project files.

---
+++ Skill_CodeFirst/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirst.Models.siniflar;
+
+namespace Skill_CodeFirst.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        //MVC işlemler genelde method içinde yapılır.
+        //Index Methodu genellikle Listeleme için kullanılır.
+        //ActionResult==>Bir dönüş tipidir.
+        public ActionResult Index()
+        {
+            CONTEXT c = new CONTEXT();
+            var degerler = c.YETENEKLERS.ToList();          
+            return View(degerler);
+        }
+    }
+}
